e5b1092a670024f529aead52105f63cee5e57682
Erik Khalimov <biomaks@gmail.com>
2019-01-30 21:45:56 -0600

    add token expiration validation, added jwt decode library

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root'
+})
 export class AuthGuardService implements CanActivate {
-  constructor(private authService: AuthService, public router: Router) {}
+  constructor(private authService: AuthService, public router: Router) {
+  }
+
   canActivate(): boolean {
     if (!this.authService.isAuthenticated()) {
       this.router.navigate(['login']);
+      return false;
+    }
+    if (this.authService.isTokenExpired()) {
       return false;
     }
     return true;
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { dashCaseToCamelCase } from '@angular/compiler/src/util';
 import { Observable } from 'rxjs';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root'
+})
 export class AuthService {
 
-  baseUrl: string = `${environment.apiUrl}/auth/signin`;
+  baseUrl = `${environment.apiUrl}/auth/signin`;
 
   constructor(private httpClient: HttpClient,
-              private router: Router) { }
-  
-  login(usernameOrEmail:string, password:string): Observable<any> {
-    return this.httpClient.post<{access_token:  string}>(this.baseUrl, {"usernameOrEmail": usernameOrEmail, "password": password})
+              private router: Router) {
+  }
+
+  login(usernameOrEmail: string, password: string): Observable<any> {
+    return this.httpClient.post<{ access_token: string }>(this.baseUrl, {'usernameOrEmail': usernameOrEmail, 'password': password});
   }
 
   logout() {
@@ -26,6 +26,31 @@ export class AuthService {
   }
 
   public isAuthenticated(): boolean {
-    return localStorage.getItem('access_token') !==  null;
+    return localStorage.getItem('access_token') !== null;
   }
+
+  getStoredToken() {
+    return localStorage.getItem('access_token');
+  }
+
+  isTokenExpired(): boolean {
+    const token = this.getStoredToken();
+    const expTime = this.getTokenExpirationTime(token);
+    if (!expTime) {
+      return true;
+    }
+    const curTime: number = +(new Date().getTime() / 1000).toFixed();
+    return curTime > expTime;
+  }
+
+  getTokenExpirationTime(token: string) {
+    const decoded = jwt_decode(token);
+    if (!decoded || decoded.exp === undefined) {
+      return null;
+    }
+    const date = new Date(0);
+    date.setUTCSeconds(decoded.exp);
+    return decoded.exp;
+  }
+
 }
